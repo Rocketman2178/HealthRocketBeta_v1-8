5ca50689e652ce4f216c9566a5dab2681ea561f6
GetAs0938 <getnetasrat0938@gmail.com>
2025-03-15 10:18:23 +0300

    add deregister connection edge function

---
+++ supabase/functions/deregister-connection/index.ts
@@ -0,0 +1,44 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import {
+  VitalClient,
+  VitalEnvironment,
+} from "https://esm.sh/@tryvital/vital-node@3.1.216";
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type",
+};
+
+serve(async (req) => {
+  // HANDLE CORS
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  try {
+    const vitalClient = new VitalClient({
+      apiKey: Deno.env.get("VITAL_API_KEY")!,
+      environment: VitalEnvironment.Sandbox,
+    });
+   const {user_id,provider} = await req.json();
+const data = await vitalClient.user.deregisterProvider(
+    user_id,
+    provider
+);
+    return new Response(
+      JSON.stringify({
+        success: true,
+        data,
+      }),
+      {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
